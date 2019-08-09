cc79f3a752a4864bef81cdc413e9ad84ef9e734a
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-08-09 11:36:42 -0400

    Created Functions, elements, classlist, setContent

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,31 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header(headerinfo) {
+    // Header
+    const header =document.createElement('div');
+    //Date
+    const headerDate = document.createElement('span');
+    //Title
+    const headerTitle = document.createElement('h1');
+    //Temp
+    const headerTemp = document.createElement('span');
+
+
+//Structure Elements
+
+header.appendChild(headerSection);
+headerDate.appendChild(date);
+headerTitle.appendChild(title);
+headerTemp.appendChild(temp);
+
+//Class Names
+header.classList.add('header');
+headerDate.classList.add('date');
+headerTemp/classList.add('temp');
+
+
+//Set Content
+
+headerSection.textContent = headerinfo
+}
